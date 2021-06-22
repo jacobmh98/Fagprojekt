6acf931c820dc5fc92704f8135c4794f10e5f150
s173470 <s173470@student.dtu.dk>
2021-06-22 13:35:38 +0200

    Fix sometimes wrong orientation on solving

--- src/model/Piece.java
+++ src/model/Piece.java
@@ -141,7 +141,7 @@ public class Piece extends Polygon {
 
 				if (i == 0) {
 					vector1 = new Double[]{corners[i] - corners[i + 2], corners[i + 1] - corners[i + 3]};
-					vector2 = new Double[]{corners[i] - corners[corners.length - 2], corners[i] - corners[corners.length - 1]};
+					vector2 = new Double[]{corners[i] - corners[corners.length - 2], corners[i + 1] - corners[corners.length - 1]};
 				} else if (i == corners.length - 2) {
 					vector1 = new Double[]{corners[i] - corners[0], corners[i + 1] - corners[1]};
 					vector2 = new Double[]{corners[i] - corners[i - 2], corners[i + 1] - corners[i - 1]};
@@ -158,7 +158,7 @@ public class Piece extends Polygon {
 
 				if (i == 0) {
 					vector1 = new Double[]{corners[i] - corners[i + 2], corners[i + 1] - corners[i + 3]};
-					vector2 = new Double[]{corners[i] - corners[corners.length - 2], corners[i] - corners[corners.length - 1]};
+					vector2 = new Double[]{corners[i] - corners[corners.length - 2], corners[i + 1] - corners[corners.length - 1]};
 				} else if (i == corners.length - 2) {
 					vector1 = new Double[]{corners[i] - corners[0], corners[i + 1] - corners[1]};
 					vector2 = new Double[]{corners[i] - corners[i - 2], corners[i + 1] - corners[i - 1]};
